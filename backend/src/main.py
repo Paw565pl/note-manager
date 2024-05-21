9f7e27d888332fe26e67a2e017d6f4b692a50b06
Paw565pl <paw565pl@gmail.com>
2024-05-21 20:53:16 +0200

    backend: create admin routes

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from src.database import db_lifespan
+from src.routes.admin import router as admin_router
 from src.routes.notes import router as notes_router
 
 app = FastAPI(lifespan=db_lifespan)
 
 app.include_router(notes_router)
+app.include_router(admin_router)
---
+++ backend/src/routes/admin.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, HTTPException, status
+from sqlalchemy import delete, select
+
+from src.auth import AdminRoleDependency
+from src.database import DatabaseDependency
+from src.models import Notes
+from src.schemas import NoteDB
+
+router = APIRouter(prefix="/api/admin")
+
+
+@router.get("/")
+async def get_all_notes(_: AdminRoleDependency, db: DatabaseDependency) -> list[NoteDB]:
+    query = select(Notes).order_by(Notes.created_at.desc())
+    result = await db.execute(query)
+    all_notes = result.scalars().all()
+
+    return all_notes
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_any_note(
+    id: int, _: AdminRoleDependency, db: DatabaseDependency
+) -> None:
+    query = delete(Notes).where(Notes.id == id)
+    result = await db.execute(query)
+    await db.commit()
+
+    deleted_rows = result.rowcount
+    if deleted_rows == 0:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+    return None
