7abea09d0c7113581955471785f6dbaeea8cd943
Paw565pl <paw565pl@gmail.com>
2024-05-21 20:52:57 +0200

    backend: create notes routes

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
 from src.database import db_lifespan
+from src.routes.notes import router as notes_router
 
 app = FastAPI(lifespan=db_lifespan)
+
+app.include_router(notes_router)
---
+++ backend/src/routes/notes.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, HTTPException, status
+from sqlalchemy import and_, delete, select
+
+from src.auth import AuthTokenDependency
+from src.database import DatabaseDependency
+from src.models import Notes
+from src.schemas import Note, NoteDB
+
+router = APIRouter(prefix="/api/notes")
+
+
+@router.get("/")
+async def get_user_notes(
+    token_info: AuthTokenDependency, db: DatabaseDependency
+) -> list[NoteDB]:
+    user_id = token_info.get("sub")
+
+    query = (
+        select(Notes).where(Notes.user_id == user_id).order_by(Notes.created_at.desc())
+    )
+    result = await db.execute(query)
+    user_notes = result.scalars().all()
+
+    return user_notes
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_note(
+    note: Note, token_info: AuthTokenDependency, db: DatabaseDependency
+) -> NoteDB:
+    user_id = token_info.get("sub")
+    user = token_info.get("preferred_username")
+    note_dict = note.model_dump()
+
+    new_note = Notes(user_id=user_id, user=user, **note_dict)
+    db.add(new_note)
+    await db.commit()
+    await db.refresh(new_note)
+
+    return new_note
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user_note(
+    id: int, token_info: AuthTokenDependency, db: DatabaseDependency
+) -> None:
+    user_id = token_info.get("sub")
+
+    query = delete(Notes).where(and_(Notes.user_id == user_id, Notes.id == id))
+    result = await db.execute(query)
+    await db.commit()
+
+    deleted_rows = result.rowcount
+    if deleted_rows == 0:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+    return None
