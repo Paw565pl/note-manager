fb11d184d63e4dd598d34c8bc9cff298498a78f8
Paw565pl <paw565pl@gmail.com>
2024-05-22 15:42:14 +0200

    frontend: create token refresh entity

---
+++ frontend/src/entites/tokenRefresh.ts
@@ -0,0 +1,10 @@
+interface TokenRefresh {
+  access_token: string;
+  expires_in: number;
+  refresh_expires_in: number;
+  refresh_token: string;
+  token_type: string;
+  id_token: string;
+}
+
+export default TokenRefresh;
