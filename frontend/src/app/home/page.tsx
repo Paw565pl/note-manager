94127ac0aae5ce4ae6874f9ae034603ea74430bd
Paw565pl <paw565pl@gmail.com>
2024-05-22 16:11:05 +0200

    frontend: add notes fetch to home page

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,14 +1,27 @@
 import { auth } from "@/auth";
+import NoteAccordion from "@/components/NoteAccordion/NoteAccordion";
 import NoteForm from "@/components/NoteForm/NoteForm";
+import Note from "@/entites/note";
+import apiService from "@/services/apiService";
 import { redirect } from "next/navigation";
 
 const HomePage = async () => {
   const session = await auth();
   if (!session) return redirect("/");
 
+  const { data: notes } = await apiService.get<Note[]>("/notes");
+
   return (
-    <main className="lg:w-1/2 mx-auto">
+    <main className="lg:w-1/2 mx-auto space-y-2">
       <NoteForm />
+      {notes.map((note) => (
+        <NoteAccordion
+          key={note.id}
+          title={note.title}
+          text={note.text}
+          created_at={note.created_at}
+        />
+      ))}
     </main>
   );
 };
