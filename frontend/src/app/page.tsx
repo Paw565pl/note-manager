1c3b42807e790837e0e30ab2d239cf5eea700d23
Paw565pl <paw565pl@gmail.com>
2024-05-22 10:32:12 +0200

    frontend: create sign in page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,31 @@
-const RootPage = () => {
-  return <div>RootPage</div>;
+import { auth, providerMap, signIn } from "@/auth";
+import { redirect } from "next/navigation";
+
+const SignInPage = async () => {
+  const session = await auth();
+  if (session) return redirect("/home");
+
+  return (
+    <main className="text-center space-y-3">
+      <h1 className="text-5xl">Welcome!</h1>
+      <h2 className="text-2xl">To continue you have to sign in.</h2>
+      <div className="flex flex-col gap-2">
+        {Object.values(providerMap).map((provider) => (
+          <form
+            key={provider.id}
+            action={async () => {
+              "use server";
+              await signIn(provider.id, { redirectTo: "/home" });
+            }}
+          >
+            <button type="submit" className="btn btn-primary">
+              <span>Sign in with {provider.name}</span>
+            </button>
+          </form>
+        ))}
+      </div>
+    </main>
+  );
 };
 
-export default RootPage;
+export default SignInPage;
