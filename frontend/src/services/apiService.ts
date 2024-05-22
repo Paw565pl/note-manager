daaf828c7a229df8050dc8a65e939a7641eb66af
Paw565pl <paw565pl@gmail.com>
2024-05-22 21:51:37 +0200

    frontend: remove setting auth header from api service

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -1,18 +1,10 @@
-import { auth } from "@/auth";
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
-apiService.interceptors.request.use(async (request) => {
-  const session = await auth();
-
-  if (session?.access_token) {
-    request.headers.Authorization = `Bearer ${session.access_token}`;
-  }
-
-  return request;
-});
+export const createAuthHeader = (accessToken: string) =>
+  `Bearer ${accessToken}`;
 
 export default apiService;
