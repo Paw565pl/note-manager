b1d95efb37435cd362526e3c4567dd8bf528d8a0
Paw565pl <paw565pl@gmail.com>
2024-05-22 10:31:23 +0200

    frontend: refactor auth config

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,12 +1,27 @@
 import NextAuth from "next-auth";
+import { Provider } from "next-auth/providers";
 import keycloak from "next-auth/providers/keycloak";
 
+const providers: Provider[] = [
+  keycloak({
+    clientId: process.env.KEYCLOAK_ID || "",
+    clientSecret: process.env.KEYCLOAK_SECRET || "",
+    issuer: process.env.KEYCLOAK_ISSUER,
+  }),
+];
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    keycloak({
-      clientId: process.env.KEYCLOAK_ID || "",
-      clientSecret: process.env.KEYCLOAK_SECRET || "",
-      issuer: process.env.KEYCLOAK_ISSUER,
-    }),
-  ],
+  providers,
+  pages: {
+    signIn: "/",
+  },
+});
+
+export const providerMap = providers.map((provider) => {
+  if (typeof provider === "function") {
+    const providerData = provider();
+    return { id: providerData.id, name: providerData.name };
+  } else {
+    return { id: provider.id, name: provider.name };
+  }
 });
