ffe7219b456c85400e0b591df35cf3b9a0faf27b
Paw565pl <paw565pl@gmail.com>
2024-05-22 21:51:57 +0200

    frontend: create hook for fetching notes

---
+++ frontend/src/hooks/useFetchNotes.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @tanstack/query/exhaustive-deps */
+import Note from "@/entites/note";
+import apiService, { createAuthHeader } from "@/services/apiService";
+import { QueryClient, useQuery } from "@tanstack/react-query";
+
+const fetchNotes = async (accessToken: string) => {
+  const { data } = await apiService.get<Note[]>("/notes", {
+    headers: { Authorization: createAuthHeader(accessToken) },
+  });
+  return data;
+};
+
+export const notesQueryKey = ["notes"];
+
+export const prefetchNotes = async (
+  queryClient: QueryClient,
+  accessToken: string
+) =>
+  queryClient.prefetchQuery({
+    queryKey: notesQueryKey,
+    queryFn: () => fetchNotes(accessToken),
+  });
+
+const useFetchNotes = (accessToken: string) =>
+  useQuery({
+    queryKey: notesQueryKey,
+    queryFn: () => fetchNotes(accessToken),
+  });
+
+export default useFetchNotes;
