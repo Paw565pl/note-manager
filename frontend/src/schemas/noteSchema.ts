a470285a83fa7c7682ea9621cbb5a0daeabef8e7
Paw565pl <paw565pl@gmail.com>
2024-05-22 10:38:14 +0200

    frontend: create note schema

---
+++ frontend/src/schemas/noteSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const noteSchema = z.object({
+  title: z
+    .string({
+      required_error: "Title is required",
+    })
+    .trim()
+    .min(10, "Title must be at least 10 characters long"),
+  text: z.string({ required_error: "Text is required" }).trim(),
+});
+
+export type NoteValues = z.infer<typeof noteSchema>;
+
+export default noteSchema;
