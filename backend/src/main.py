66bec9997ff5c4ec10eb8b0a91b5f8efb91ac562
Paw565pl <paw565pl@gmail.com>
2024-05-22 09:41:20 +0200

    backend: add brotli middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,4 @@
+from brotli_asgi import BrotliMiddleware
 from fastapi import FastAPI
 
 from src.database import db_lifespan
@@ -5,6 +6,7 @@ from src.routes.admin import router as admin_router
 from src.routes.notes import router as notes_router
 
 app = FastAPI(lifespan=db_lifespan)
+app.add_middleware(BrotliMiddleware)
 
 app.include_router(notes_router)
 app.include_router(admin_router)
