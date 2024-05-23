2a78037f2ecbe4f61f177877a29ed44e14f776d0
Paw565pl <paw565pl@gmail.com>
2024-05-23 09:29:23 +0200

    frontend: create hook for deleting notes as admin

---
+++ frontend/src/hooks/useAdminDeleteNote.ts
@@ -0,0 +1,28 @@
+import Note from "@/entites/note";
+import apiService, { createAuthHeader } from "@/services/apiService";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError, AxiosResponse } from "axios";
+import { adminNotesQueryKey } from "./useAdminFetchNotes";
+import { notesQueryKey } from "./useFetchNotes";
+
+const adminDeleteNote = async (id: number, accessToken: string) => {
+  const response = await apiService.delete(`/admin/${id}`, {
+    headers: { Authorization: createAuthHeader(accessToken) },
+  });
+  return response;
+};
+
+const useAdminDeleteNote = (accessToken: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation<AxiosResponse, AxiosError, number, Note[]>({
+    mutationKey: ["deleteNote"],
+    mutationFn: (id) => adminDeleteNote(id, accessToken),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: adminNotesQueryKey });
+      queryClient.invalidateQueries({ queryKey: notesQueryKey });
+    },
+  });
+};
+
+export default useAdminDeleteNote;
