60ca5c82e9133af9d116ff5a05eb73566a310277
Paw565pl <paw565pl@gmail.com>
2024-05-24 20:58:05 +0200

    frontend: fix schema to match backend

--- frontend/src/schemas/noteSchema.ts
+++ frontend/src/schemas/noteSchema.ts
@@ -6,7 +6,8 @@ const noteSchema = z.object({
       required_error: "Title is required",
     })
     .trim()
-    .min(10, "Title must be at least 10 characters long"),
+    .min(10, "Title must be at least 10 characters long")
+    .max(100, "Title must be at most 100 characters long"),
   text: z.string({ required_error: "Text is required" }).trim(),
 });
 
