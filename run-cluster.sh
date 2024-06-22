0acfdc9c0fdc15815190e853c5b285ea7de1f428
Paw565pl <paw565pl@gmail.com>
2024-06-22 19:41:48 +0200

    update run cluster script

--- run-cluster.sh
+++ run-cluster.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 docker build -t note-manager-keycloak:latest ./keycloak
+docker build -t note-manager-prometheus:latest ./prometheus
+docker build -t note-manager-grafana:latest ./grafana
 docker build -t note-manager-backend:latest ./backend
 docker build -t note-manager-frontend:latest ./frontend
 
