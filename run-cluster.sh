cd1e35be159f6a69f2ae5de6a62ee1c052ac684d
Paw565pl <paw565pl@gmail.com>
2024-09-04 11:04:20 +0200

    frontend: rename dockerfile

--- run-cluster.sh
+++ run-cluster.sh
@@ -4,6 +4,6 @@ docker build -t note-manager-keycloak:latest ./keycloak
 docker build -t note-manager-prometheus:latest ./prometheus
 docker build -t note-manager-grafana:latest ./grafana
 docker build -t note-manager-backend:latest ./backend
-docker build -t note-manager-frontend:latest ./frontend
+docker build -t note-manager-frontend:latest -f ./frontend/Dockerfile.prod ./frontend
 
 kubectl apply -f kubernetes/
